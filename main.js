ae75ac0068b078f5524d7b0cda6f564b1349eced
Ian Arawjo <fatso784@gmail.com>
2021-08-19 17:36:02 -0400

    Add license notice to top of main script

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// Notate: Jupyter Notebook Extension for writing/drawing inside an iPython notebook.
+// Copyright (C) 2021 Ian Arawjo
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License v3 as published by
+// the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License v3 for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License v3
+// along with this program.  If not, see <https://www.gnu.org/licenses/lgpl-3.0.en.html>.
+
 define([
     'require',
     'jquery',
